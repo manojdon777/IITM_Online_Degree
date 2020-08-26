d17c4e2a1730368f8e770ce29d0f6002afdb72a3
manojdon777 <manojdon777@gmail.com>
2020-08-26 16:32:21 +0530

    Update problem_1.py
    No need of of if else or try catch while updating list as value in dictionary

--- submission/problem_1.py
+++ submission/problem_1.py
@@ -1,8 +1,7 @@
-#9791563725
-def checkAnagram():
-	for testcases in range(int(input())):
-		string1 = input()
-		string2 = input()
+def checkAnagram(string1, string2):
+	# for testcases in range(int(input())):
+		# string1 = input()
+		# string2 = input()
 		string1 = string1.replace(' ', '')
 		string2 = string2.replace(' ', '')
 		returnChar = 'N'
@@ -11,21 +10,14 @@ def checkAnagram():
 		if length1 == length2:
 			dictionary1 = {}	
 			dictionary2 = {}
-			key1 = dictionary1.keys()
-			key2 = dictionary2.keys()
 			for n in string1:
-			    if n in key1:
-			        dictionary1[n] += 1
-			    else:
-			        dictionary1[n] = 1
+			    dictionary1[n] = dictionary1.get(n, 0) + 1
 			for n in string2:
-			    if n in key2:
-			        dictionary2[n] += 1
-			    else:
-			        dictionary2[n] = 1
-			if dictionary1 == dictionary2:
-				returnChar = 'Y'
+			    dictionary2[n] = dictionary2.get(n, 0) + 1
+
+		if dictionary1 == dictionary2:
+			returnChar = 'Y'
 		print(dictionary1)
 		print(dictionary2)
 		print(returnChar)
-checkAnagram()
+checkAnagram('xaa','aax')
